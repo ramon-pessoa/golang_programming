2a2513f74ab4f09654e714996671d46a9b4e15ad
ramonpessoa <ramon@ordigraphe.com>
2019-03-15 21:53:39 -0400

    Dictionaries on Golang

--- go/src/arrays/arrays.go
+++ go/src/arrays/arrays.go
@@ -14,4 +14,23 @@ func main() {
 	b := [5]int{5, 4, 3, 2, 1}
 
 	fmt.Println(b)
+
+	// slices
+	c := []int{1, 2, 3, 4, 5}
+	c = append(c, 13)
+
+	fmt.Println(c)
+
+	// dictionary
+	vertices := make(map[string]int)
+
+	vertices["vertices"] = 2
+	vertices["square"] = 3
+	vertices["dodecagon"] = 12
+
+	fmt.Println(vertices)
+
+	delete(vertices, "square")
+
+	fmt.Println(vertices)
 }
